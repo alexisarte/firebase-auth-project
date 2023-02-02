11f4335ed34c001f4942616825e7ffb38fea5b5f
alexis <alexisrodriguezarteaga@gmail.com>
2023-02-02 18:54:04 -0300

    Registration form

---
+++ src/app/signupForm.js
@@ -0,0 +1,7 @@
+const signupForm = document.querySelector('.signup-form')
+
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = signupForm['#signup-email'].value;
+  const password = signupForm['#signup-password'].value;
+});
