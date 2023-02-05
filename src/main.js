4287f907bb5a27136e43619f2bcdcfb6aca97a85
alexis <alexisrodriguezarteaga@gmail.com>
2023-02-05 12:30:27 -0300

    onAuthStateChanged

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
+import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js';
+import { auth } from './app/firebase.js';
+import { loginCheck } from './app/loginCheck.js';
+
 import './app/signupForm.js';
-console.log('main.js');
+import './app/logout.js';
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    loginCheck(user);
+  } else {
+    loginCheck(user);
+  }
+});
